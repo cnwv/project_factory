2f2bf3bafbeff931ea35d42b8bca3698da4e9ee8
cnwv <115669384+cnwv@users.noreply.github.com>
2023-10-05 00:30:47 +0400

    initial commit

---
+++ project_factory/authapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
+
+
+# Create your models here.
+class User(AbstractUser):
+    pass
